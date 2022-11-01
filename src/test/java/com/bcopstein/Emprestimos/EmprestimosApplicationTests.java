fc17b3d66fbe4c111078b7b4d2b3005bc191b092
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2022-11-01 18:36:34 -0300

    chore3

--- src/test/java/com/bcopstein/Emprestimos/EmprestimosApplicationTests.java
+++ src/test/java/com/bcopstein/Emprestimos/EmprestimosApplicationTests.java
@@ -3,18 +3,31 @@ package com.bcopstein.Emprestimos;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.LinkedHashMap;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@WebMvcTest(EmprestimoController.class)
+//@AutoConfigureMockMvc
 class EmprestimosApplicationTests {
     @Autowired
     private EmprestimosApplication controller;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @LocalServerPort
     private int port;
@@ -68,6 +81,7 @@ class EmprestimosApplicationTests {
         assertThat(receivedTotalShare).isEqualTo((totalShareExpected));
     }
 
+    @Test
     public void testCompositeFees() throws Exception {
         double valor = 20000;
         double taxa = 2.5;
@@ -86,6 +100,12 @@ class EmprestimosApplicationTests {
 
         assertThat(totalValueExpected).isEqualTo((receivedTotalValue));
         assertThat(receivedTotalShare).isEqualTo((totalShareExpected));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
     }
 
 }
