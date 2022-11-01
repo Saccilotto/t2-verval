f8095acad3cb477d68b7c5af9b4065359b19911e
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2022-11-01 18:17:44 -0300

    chore2

--- src/test/java/com/bcopstein/Emprestimos/EmprestimosApplicationTests.java
+++ src/test/java/com/bcopstein/Emprestimos/EmprestimosApplicationTests.java
@@ -19,12 +19,32 @@ class EmprestimosApplicationTests {
     @LocalServerPort
     private int port;
 
+    public static final double TXSEGUROPADRAO = 0.01;
+    public static final double IOF = 0.06;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() throws Exception {
         assertThat(controller).isNotNull();
+    }
+
+    private double calculateSimpleFee(double valor, double taxa, int nroParcelas) {
+        double valorIof = valor * IOF;
+        return valor + valorIof + (valor * (taxa + TXSEGUROPADRAO) * nroParcelas);
+    }
+
+    private double calculateCompositeFees(double valor, double taxa, int nroParcelas) {
+        double tx = taxa;
+        double valorIof = valor * IOF;
+        tx += TXSEGUROPADRAO;
+        double valorAcum = valor;
+        while (nroParcelas > 0) {
+            valorAcum += valorAcum * tx;
+            nroParcelas--;
+        }
+        return valor + valorIof + (valorAcum - valor);
     }
 
     @Test
@@ -38,24 +58,34 @@ class EmprestimosApplicationTests {
         LinkedHashMap response = this.restTemplate.getForObject(buildedUrl,
                 LinkedHashMap.class);
 
-        // TODO CALCULAR O VALOR BASEADO NA ENTRADA
-        Double totalValueExpected = 523199.99999999994;
-        Double totalShareExpected = 52319.99999999999;
+        Double totalValueExpected = this.calculateSimpleFee(valor, taxa, nroParcelas);
+        Double totalShareExpected = totalValueExpected / nroParcelas;
 
-        Double receivedTotalValue = (Double)response.get("valorTotal");
-        Double receivedTotalShare = (Double)response.get("valorParcela");
+        Double receivedTotalValue = (Double) response.get("valorTotal");
+        Double receivedTotalShare = (Double) response.get("valorParcela");
 
         assertThat(totalValueExpected).isEqualTo((receivedTotalValue));
         assertThat(receivedTotalShare).isEqualTo((totalShareExpected));
     }
 
     public void testCompositeFees() throws Exception {
-        var x = this.restTemplate.getForObject("http://localhost:" + port + "/emprestimo/jurosCompostos?valor=2000&parcelas=10&taxa=1.5",
-                Object.class);
+        double valor = 20000;
+        double taxa = 2.5;
+        int nroParcelas = 10;
 
+        String buildedUrl = "http://localhost:" + port + "/emprestimo/jurosCompostos?valor=" + valor + "&parcelas=" + nroParcelas + "&taxa=" + taxa;
 
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/emprestimo/jurosCompostos?valor=2000&parcelas=10&taxa=1.5",
-                String.class)).contains("Hello, World");
+        LinkedHashMap response = this.restTemplate.getForObject(buildedUrl,
+                LinkedHashMap.class);
+
+        Double totalValueExpected = this.calculateCompositeFees(valor, taxa, nroParcelas);
+        Double totalShareExpected = totalValueExpected / nroParcelas;
+
+        Double receivedTotalValue = (Double) response.get("valorTotal");
+        Double receivedTotalShare = (Double) response.get("valorParcela");
+
+        assertThat(totalValueExpected).isEqualTo((receivedTotalValue));
+        assertThat(receivedTotalShare).isEqualTo((totalShareExpected));
     }
 
 }
